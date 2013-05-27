ba0c5a43cb98434036ae44c7265bf243ea8592f8
KAZUMA Ukyo <yaakaito@gmail.com>
2013-05-28 04:54:34 +0900

    Add flatMap, flatten. And fix tests bugs.

--- src/option.ts
+++ src/option.ts
@@ -5,6 +5,8 @@ module Katana {
         getOrElse(defaultValue: () => A): A;
         match(some:(value: A) => any, none:() => any);
         map<B>(f: (value: A) => B): Option<B>;
+        flatMap<B>(f: (value: A) => Option<B>): Option<B>;
+        flatten<B>(): Option<B>;
     }
 
     export class Some<A> implements Option<A> {
@@ -27,8 +29,26 @@ module Katana {
             return new Some<B>(f(this.get()));
         }
 
+        flatMap<B>(f: (value: A) => Option<B>): Option<B> {
+            return f(this.get());
+        }
 
+        flatten<B>(): Option<B> {
+            if (this.value instanceof Some) {
+                var v = <Some<B>>this.value;
+                return new Some<B>(v.get());
+            }
+            else if (this.value instanceof None) {
+                return new None<B>();
+            }
+            else {
+                throw new Error('Cannot prove that.');
+            }
+            return null;
+        }
+        
     }
+    
 
     export class None<A> implements Option<A> {
 
@@ -47,5 +67,14 @@ module Katana {
         map<B>(f: (value: A) => B): Option<B> {
             return new None<B>();
         }
+
+        flatMap<B>(f: (value: A) => Option<B>): Option<B> {
+            return new None<B>();
+        }
+
+        flatten<B>(): Option<B> {
+            return new None<B>();
+        }
     }
+    
 }
--- test/option.ts
+++ test/option.ts
@@ -7,8 +7,8 @@ describe('Option', () => {
 
     describe('Some', () => {    
         
-        var some;
-        beforeEach(() => { some = new Katana.Some('value')});
+        var some: Katana.Some<string>;
+        beforeEach(() => { some = new Katana.Some('value') });
 
         describe('#get', () => {
             it('returns the value that binding on constructor', () => {
@@ -39,16 +39,44 @@ describe('Option', () => {
         });
 
         describe('#map', () => {
-            it('can convert the value with convertion function', () => {
-                some.map(v => 1).get().should.to.equal(1);
-            });  
+            it('returns a Some containing the result of applying func', () => {
+                some.map((v) => 1).get().should.equal(1);
+            });
         });
+
+        describe('#flatMap', () => {
+            it('returns the result of applying func if it is a Some', () => {
+                var mapped = some.flatMap(v => new Katana.Some(1));
+                mapped.should.be.instanceof(Katana.Some);
+                mapped.get().should.equal(1);
+            });
+
+            it('returns a None if result of applying func that is None', () => {
+                some.flatMap(v => new Katana.None<number>()).should.be.instanceof(Katana.None);
+            });
+        });
+
+        describe('#flatten', () => {
+            it('returns a Some if its value is Same', () => {
+                var flat = some.map(v => new Katana.Some('a')).flatten();
+                flat.should.be.instanceof(Katana.Some);
+                flat.get().should.equal('a');
+            });
+
+            it('returns None if its value is None', () => {
+                some.map(v => new Katana.None<string>()).flatten().should.be.instanceof(Katana.None);    
+            })
+
+            it('cannot prove if its value isnt Option', () => {
+                some.flatten.should.throw('Cannot prove that.');
+            });
+        })
     });
 
     describe('None', () => {
         
-        var none;
-        beforeEach(() => { none = new Katana.None<string>()});
+        var none: Katana.None<string>;
+        beforeEach(() => { none = new Katana.None<string>() });
 
         describe('#get', () => {
             it('throws No such element Exception', () => {
@@ -77,8 +105,20 @@ describe('Option', () => {
         });
 
         describe('#map', () => {
-            it('never do anything else but create new None', () => {
+            it('never do anything', () => {
                 none.map(v => 1).should.be.instanceof(Katana.None);
+            });
+        });
+
+        describe('#flatMap', () => {
+            it('never do anything', () => {
+                none.flatMap(v => new Katana.Some(1)).should.be.instanceof(Katana.None);
+            });
+        });
+
+        describe('#flatten', () => {
+            it('never do anything', () => {
+                none.flatten().should.be.instanceof(Katana.None);
             });
         });
     });
