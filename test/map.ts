e47736f07f1ea7fe52057b01a041e5338f19a59d
KAZUMA Ukyo <yaakaito@gmail.com>
2013-06-03 19:25:20 +0900

    Impl Map#constructor and Add map tests

---
+++ test/map.ts
@@ -0,0 +1,44 @@
+/// <reference path="../src/map.ts" />
+/// <reference path="../d.ts/mocha.d.ts" />
+/// <reference path="../d.ts/chai.d.ts" />
+
+module Katana.Spec {
+
+    describe('Map', () => {
+        chai.should();
+
+        describe('#constructor', () => {
+
+            it('can initialize with key/value pairs', () => {
+                var map = new Katana.Map('key1', 'value1',
+                                         'key2', 'value2');
+
+                map.get('key1').get().should.be.equal('value1');
+                map.get('key2').get().should.be.equal('value2');
+            });
+
+            it('cannot initialize with invalid key/value paires', () => {
+                var f = () => {
+                    new Katana.Map('key1', 'value1',
+                                   'key2');
+                }
+                f.should.throw('key2 has not value.');
+            });
+
+            it('can initialize with Object', () => {
+                var map = new Katana.Map<string, string>({
+                    'key1': 'value1',
+                    'key2': 'value2'
+                });
+
+                map.get('key1').get().should.be.equal('value1');
+                map.get('key2').get().should.be.equal('value2');
+            });
+
+            it('can initialize with any arugment', () => {
+                var map = new Katana.Map<string, string>();
+                map.get('hoge').get.should.throw('No such element.');   
+            });
+        });
+    });    
+}
