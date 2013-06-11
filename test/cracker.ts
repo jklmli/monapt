8e9943340c53f175bf68b6a257aad9b8e8169439
KAZUMA Ukyo <yaakaito@gmail.com>
2013-06-11 14:58:06 +0900

    Add functions Cracker

---
+++ test/cracker.ts
@@ -0,0 +1,53 @@
+/// <reference path="../src/cracker.ts" />
+/// <reference path="../d.ts/mocha.d.ts" />
+/// <reference path="../d.ts/chai.d.ts" />
+
+module Katana.Spec {
+    chai.should();
+
+    interface ITestFunc {
+        (): void;
+    }
+
+    interface ITestFunc2 {
+        (v: number): void;
+    }
+
+    describe('Cracker', () => {
+        it('can fire callbacks', () => {
+            var cracker = new Katana.Cracker<ITestFunc>();
+            var counter = 0;
+            cracker.add(() => {
+                counter++;
+            });
+            cracker.add(() => {
+                counter++;    
+            });
+            counter.should.equal(0);
+            
+            cracker.fire(fn => fn());
+            counter.should.equal(2);
+
+            cracker.add(() => {
+                counter++;    
+            });
+            counter.should.equal(3);
+        });
+
+        it('should apply function with binding context', () => {
+            var cracker = new Katana.Cracker<ITestFunc2>();
+            var counter = 0;
+
+            cracker.add(num => counter += num);
+            cracker.add(num => counter += num);
+
+            cracker.fire(fn => fn(2));
+
+            counter.should.equal(4);
+
+            cracker.add(num => counter += num * 2);
+            counter.should.equal(8);
+
+        });
+    });
+}
