8b30032be7d4d3d4170b3686ce0482e3f3678a2d
Jiawei Li <jiawei.h.li@gmail.com>
2014-12-02 16:18:10 -0800

    rebuild module

---
+++ dist/monapt.d.ts
@@ -0,0 +1,180 @@
+declare module monapt {
+    interface Tuple1<A> {
+        _1: A;
+    }
+    var Tuple1: <A>(a: A) => Tuple1<A>;
+    interface Tuple2<A, B> {
+        _1: A;
+        _2: B;
+    }
+    var Tuple2: <A, B>(a: A, b: B) => Tuple2<A, B>;
+}
+declare module monapt {
+    interface IOptionMatcher<A> {
+        Some?(value: A): void;
+        None?(): void;
+    }
+    var Option: <T>(value: T) => Option<T>;
+    interface Option<A> {
+        isEmpty: boolean;
+        get(): A;
+        getOrElse(defaultValue: A): A;
+        orElse(alternative: Option<A>): Option<A>;
+        match(matcher: IOptionMatcher<A>): any;
+        map<B>(f: (value: A) => B): Option<B>;
+        flatMap<B>(f: (value: A) => Option<B>): Option<B>;
+        filter(predicate: (value: A) => boolean): Option<A>;
+        reject(predicate: (value: A) => boolean): Option<A>;
+        foreach(f: (value: A) => void): void;
+    }
+    class Some<A> implements Option<A> {
+        private value;
+        isEmpty: boolean;
+        constructor(value: A);
+        get(): A;
+        getOrElse(defaultValue: A): A;
+        orElse(alternative: Option<A>): Option<A>;
+        match(matcher: IOptionMatcher<A>): void;
+        map<B>(f: (value: A) => B): Option<B>;
+        flatMap<B>(f: (value: A) => Option<B>): Option<B>;
+        filter(predicate: (value: A) => boolean): Option<A>;
+        reject(predicate: (value: A) => boolean): Option<A>;
+        foreach(f: (value: A) => void): void;
+    }
+    var None: Option<any>;
+}
+declare module monapt {
+    interface ITryMatcher<T> {
+        Success?(value: T): void;
+        Failure?(error: Error): void;
+    }
+    interface Try<T> {
+        isSuccess: boolean;
+        isFailure: boolean;
+        get(): T;
+        getOrElse(defaultValue: () => T): T;
+        orElse(alternative: () => Try<T>): Try<T>;
+        match(matcher: ITryMatcher<T>): any;
+        map<U>(f: (value: T) => U): Try<U>;
+        flatMap<U>(f: (value: T) => Try<U>): Try<U>;
+        filter(predicate: (value: T) => boolean): Try<T>;
+        reject(predicate: (value: T) => boolean): Try<T>;
+        foreach(f: (value: T) => void): void;
+        recover(fn: (error: Error) => T): Try<T>;
+        recoverWith(fn: (error: Error) => Try<T>): Try<T>;
+    }
+    class Success<T> implements Try<T> {
+        private value;
+        isSuccess: boolean;
+        isFailure: boolean;
+        constructor(value: T);
+        get(): T;
+        getOrElse(defaultValue: () => T): T;
+        orElse(alternative: () => Try<T>): Try<T>;
+        match(matcher: ITryMatcher<T>): void;
+        map<U>(f: (value: T) => U): Try<U>;
+        flatMap<U>(f: (value: T) => Try<U>): Try<U>;
+        filter(predicate: (value: T) => boolean): Try<T>;
+        reject(predicate: (value: T) => boolean): Try<T>;
+        foreach(f: (value: T) => void): void;
+        recover(fn: (error: Error) => T): Try<T>;
+        recoverWith(fn: (error: Error) => Try<T>): Try<T>;
+    }
+    class Failure<T> implements Try<T> {
+        private error;
+        isSuccess: boolean;
+        isFailure: boolean;
+        constructor(error: Error);
+        get(): T;
+        getOrElse(defaultValue: () => T): T;
+        orElse(alternative: () => Try<T>): Try<T>;
+        match(matcher: ITryMatcher<T>): void;
+        map<U>(f: (value: T) => U): Try<U>;
+        flatMap<U>(f: (value: T) => Try<U>): Try<U>;
+        filter(predicate: (value: T) => boolean): Try<T>;
+        reject(predicate: (value: T) => boolean): Try<T>;
+        foreach(f: (value: T) => void): void;
+        recover(fn: (error: Error) => T): Try<T>;
+        recoverWith(fn: (error: Error) => Try<T>): Try<T>;
+    }
+    var Try: <T>(f: () => T) => Try<T>;
+}
+declare module monapt {
+    interface ICrackerProducer<F> {
+        (f: F): void;
+    }
+    class Cracker<F extends Function> {
+        private fired;
+        private callbacks;
+        private producer;
+        fire(producer: ICrackerProducer<F>): void;
+        private fireAll();
+        add(fn: F): void;
+    }
+}
+declare module monapt {
+    interface ICompleteFunction<T> {
+        (trier: Try<T>): void;
+    }
+    interface IFutureSuccess<T> {
+        (value: T): void;
+    }
+    interface IFutureFailure<T> {
+        (error: Error): void;
+    }
+    interface IFuturePromiseLike<T> {
+        (value: T): void;
+        (error: Error): void;
+        success: IFutureSuccess<T>;
+        failure: IFutureFailure<T>;
+    }
+    class Future<T> {
+        private cracker;
+        constructor(future: (promise: IFuturePromiseLike<T>) => void);
+        static succeed<T>(value: T): Future<T>;
+        static failed<T>(error: Error): Future<T>;
+        success(value: T): void;
+        failure(error: Error): void;
+        onComplete(callback: ICompleteFunction<T>): void;
+        onSuccess(callback: (value: T) => void): void;
+        onFailure(callback: (error: Error) => void): void;
+        map<U>(f: (value: T, promise: IFuturePromiseLike<U>) => void): Future<U>;
+        flatMap<U>(f: (value: T) => Future<U>): Future<U>;
+        filter(predicate: (value: T) => boolean): Future<T>;
+        reject(predicate: (value: T) => boolean): Future<T>;
+        recover(fn: (e: Error, promise: IFuturePromiseLike<T>) => void): Future<T>;
+        recoverWith(fn: (e: Error) => Future<T>): Future<T>;
+    }
+    class Promise<T> extends Future<T> {
+        isComplete: boolean;
+        constructor();
+        success(value: T): void;
+        failure(error: Error): void;
+        future(): Future<T>;
+    }
+    var future: <T>(f: (promise: IFuturePromiseLike<T>) => void) => Future<T>;
+}
+declare module monapt {
+    interface IHashable {
+        hash?(): string;
+    }
+    class Map<K extends IHashable, V> {
+        private real;
+        private selector;
+        constructor(key: K, value: V, ...keysAndValues: any[]);
+        constructor(raw: Object);
+        constructor();
+        private ensureSelector(hint?);
+        private add(key, value);
+        foreach(f: (key: K, value: V) => void): void;
+        map<K2, V2>(f: (key: K, value: V) => Tuple2<K2, V2>): Map<K2, V2>;
+        flatMap<K2, V2>(f: (key: K, value: V) => Map<K2, V2>): Map<K2, V2>;
+        mapValues<U>(f: (value: V) => U): Map<K, U>;
+        filter(predicate: (key: K, value: V) => boolean): Map<K, V>;
+        reject(predicate: (key: K, value: V) => boolean): Map<K, V>;
+        find(f: (key: K, value: V) => boolean): Option<Tuple2<K, V>>;
+        get(key: K): Option<V>;
+        getOrElse(key: K, defaultValue: V): V;
+        head(): Option<Tuple2<K, V>>;
+    }
+}
