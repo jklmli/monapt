d3da2708a7c02b29ac724f14e9e94c14b87e5d05
Kevin Li <jiawei.h.li@gmail.com>
2016-12-31 17:24:33 -0800

    build(webpack): support typescript compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     library: 'Monapt',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ],
+    extensions: ['.ts', '.js']
+  },
 };
