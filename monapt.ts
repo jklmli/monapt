8ec638108cb945f9d9c4d8c9d697e0b6ddf6ab31
Kevin Li <jiawei.h.li@gmail.com>
2017-06-30 01:09:08 -0700

    chore: delete everything
    Start with a clean workspace for 1.0.

--- monapt.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./src/tuple.ts" />
-/// <reference path="./src/option.ts" />
-/// <reference path="./src/try.ts" />
-/// <reference path="./src/cracker.ts" />
-/// <reference path="./src/future.ts" />
-/// <reference path="./src/map.ts" />
